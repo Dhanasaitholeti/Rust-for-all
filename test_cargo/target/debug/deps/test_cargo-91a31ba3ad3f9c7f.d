ad77311e3926b444769f0edff848b18340533f7c
Dhanasai Tholeti <dhanasai5566@gmail.com>
2023-04-09 22:31:39 +0530

    added the cargo tutorial

---
+++ test_cargo/target/debug/deps/test_cargo-91a31ba3ad3f9c7f.d
@@ -0,0 +1,5 @@
+D:\programming space\rust\Rust-for-all\test_cargo\target\debug\deps\test_cargo-91a31ba3ad3f9c7f.exe: src\main.rs
+
+D:\programming space\rust\Rust-for-all\test_cargo\target\debug\deps\test_cargo-91a31ba3ad3f9c7f.d: src\main.rs
+
+src\main.rs:
---
+++ test_cargo/target/debug/test_cargo.d
@@ -0,0 +1,1 @@
+D:\programming\ space\rust\Rust-for-all\test_cargo\target\debug\test_cargo.exe: D:\programming\ space\rust\Rust-for-all\test_cargo\src\main.rs
